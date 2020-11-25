3d6f35e74ce9a2035d14cf27c0398761f9a043b2
CV145 <carlosvaleriano45@yahoo.com>
2020-11-25 15:53:11 -0600

    Progress made on phases 3 and 4

--- cdkexample.cc
+++ cdkexample.cc
@@ -1,8 +1,8 @@
 /*
  * Filename:            cdkexample.cc
  * Date:                11/08/2020
- * Author:              Stephen Perkins
- * Email:               stephen.perkins@utdallas.edu
+ * Author:              Carlos Valeriano
+ * Email:               cdv190000@utdallas.edu
  * Version:             1.0
  * Copyright:           2020, All Rights Reserved
  *
@@ -13,7 +13,9 @@
  */
 
 #include <iostream>
+#include <sstream>
 #include "cdk.h"
+#include "header.h"
 
 /*
  * For grins and giggles, we will define values using the C
@@ -22,10 +24,10 @@
  * The names are replaced by their values when seen later in the code.
  */
 
-#define MATRIX_ROWS 6
+#define MATRIX_ROWS 5
 #define MATRIX_COLS 3
 #define BOX_WIDTH 15
-#define MATRIX_NAME_STRING "Test Matrix"
+#define MATRIX_NAME_STRING "Binary File Contents"
 
 using namespace std;
 
@@ -37,13 +39,15 @@ int main()
   CDKSCREEN	*cdkscreen;
   CDKMATRIX     *myMatrix;           // CDK Screen Matrix
 
+  BinaryFileHeader* header = getHeader();
+
   // CDK uses offset 1 and C/C++ use offset 0.  Therefore, we create one more 
   // slot than necessary and ignore the value at location 0.
-  const char 		*rowTitles[MATRIX_ROWS+1] = {"IGNORE", "R1", "R2", "R3", "R4", "R5", "R6"};
-  const char 		*columnTitles[MATRIX_COLS+1] = {"IGNORE", "C1", "C2", "C3"};
+  const char 		*rowTitles[MATRIX_ROWS+1] = {"IGNORE", "a", "b", "c", "d", "e"};
+  const char 		*columnTitles[MATRIX_COLS+1] = {"IGNORE", "a", "b", "c"};
   int		colWidths[MATRIX_COLS+1] = {BOX_WIDTH, BOX_WIDTH, BOX_WIDTH, BOX_WIDTH};
   int		boxTypes[MATRIX_COLS+1] = {vMIXED, vMIXED, vMIXED, vMIXED};
-
+ 
   /*
    * Initialize the Cdk screen.
    *
@@ -74,11 +78,49 @@ int main()
   /*
    * Dipslay a message
    */
-  setCDKMatrixCell(myMatrix, 2, 2, "Test Message");
+  
+  string temp = "";
+  
+  stringstream aa;
+  aa << "Magic: " << header->magicNumber; 
+  setCDKMatrixCell(myMatrix, 1, 1, aa.str().c_str());
+
+  stringstream ab;
+  ab << "Version: " <<  header->versionNumber;
+  setCDKMatrixCell(myMatrix, 1, 2, ab.str().c_str());
+
+  stringstream ac;
+  ac << "NumRecords: " << header->numRecords;
+  setCDKMatrixCell(myMatrix, 1, 3, ac.str().c_str());
+
+  //use for loop to do the file records
+  int maxCount;
+  if (header->numRecords > 4)
+    maxCount=4;
+  else
+    maxCount=header->numRecords;
+
+
+  for (int i = 0; i < maxCount; i++)
+    {
+      BinaryFileRecord* record = getRecord();
+
+      stringstream x;
+      x << "strlen: " << record->strLength;
+      setCDKMatrixCell(myMatrix, 2+i, 1, x.str().c_str());
+
+      stringstream y;
+      y << record->stringBuffer;
+      setCDKMatrixCell(myMatrix, 2+i, 2, y.str().c_str());
+    }
+
   drawCDKMatrix(myMatrix, true);    /* required  */
 
+
+
+
   /* so we can see results */
-  sleep (10);
+   sleep (30);
 
 
   // Cleanup screen
